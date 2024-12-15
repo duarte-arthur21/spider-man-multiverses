df76d23a72958c368426b706742abb1a6fbf1c10
duarte-arthur21 <ferreiraduarte21@gmail.com>
2024-12-14 21:03:24 -0300

    Criação da função de rotação do carrossel 3D

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -10,6 +10,30 @@ function handleMouseLeave() {
 
 function addEventListenersToCards() {
     const cardElements = document.getElementsByClassName('s-card');
+
+    for (let index = 0; index < cardElements.length; index++ ) {
+        const card = cardElements[index];
+        card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+
+function selectCarouselItem(selectedButtonElement) {
+    const selectCarouselItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.s-cards-carousel');
+    const transform = carousel.style.transform;
+    const rotateY = transform.maych(/rotateY\((-?d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    const newTransform = transform.replace(rotateY[0], `rotateT(${rotateYDeg}deg)`);
+
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector('.s-controller__button--active');
+    activeButtonElement.classList.remove('.s-controller__button--active');
+    selectedButtonElement.classList.add('.s-controller__button--active');
+
 }
 
 function addEventListenersToCards(){
